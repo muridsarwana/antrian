5bde92989b960ead4a05701db252f0fbf6d10567
muridsarwana <muridsarwana@gmail.com>
2024-05-13 15:22:54 +0700

    com2 edit nomor-antrian

--- nomor-antrian/index.php
+++ nomor-antrian/index.php
@@ -43,27 +43,43 @@
     <div class="container pt-5">
       <div class="row justify-content-lg-center">
         <div class="col-lg-5 mb-4">
-          <div class="px-4 py-3 mb-4 bg-white rounded-2 shadow-sm">
-            <!-- judul halaman -->
-            <div class="d-flex align-items-center me-md-auto">
-              <i class="bi-people-fill text-success me-3 fs-3"></i>
-              <h1 class="h5 pt-2">Nomor Antrian</h1>
-            </div>
-          </div>
+        <div class="px-4 py-3 mb-4 bg-white rounded-2 shadow-sm">
+  <!-- judul halaman -->
+  <div class="d-flex align-items-center me-md-auto">
+    <i class="bi-people-fill text-success me-3 fs-3"></i>
+    <h1 class="h5 pt-2">Nomor Antrian</h1>
+  </div>
+</div>
 
-          <div class="card border-0 shadow-sm">
-            <div class="card-body text-center d-grid p-5">
-              <div class="border border-success rounded-2 py-2 mb-5">
-                <h3 class="pt-4">ANTRIAN</h3>
-                <!-- menampilkan informasi jumlah antrian -->
-                <h1 id="antrian" class="display-1 fw-bold text-success text-center lh-1 pb-2"></h1>
-              </div>
-              <!-- button pengambilan nomor antrian -->
-              <a id="insert" href="javascript:void(0)" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">
-                <i class="bi-person-plus fs-4 me-2"></i> Ambil Nomor
-              </a>
-            </div>
-          </div>
+<!-- Queue Section 1. Sekretariat -->
+<div class="card border-0 shadow-sm">
+  <div class="card-body text-center d-grid p-3">
+    <div class="mb-3 fw-bold fs-3"><span class="text-success">ANTRIAN</span> SEKRETARIAT</div>
+    <div class="border border-success rounded-2 py-2 mb-4">
+      <!-- menampilkan informasi jumlah antrian -->
+      <h1 id="antrian" class="display-1 fw-bold text-success text-center lh-1 pb-2"></h1>
+    </div>
+    <!-- button pengambilan nomor antrian -->
+    <a id="insert" href="javascript:void(0)" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">
+      <i class="bi-person-plus fs-4 me-2"></i> Ambil Nomor
+    </a>
+  </div>
+</div>
+
+<!-- Queue Section 2. PSMA -->
+<div class="card border-0 shadow-sm">
+  <div class="card-body text-center d-grid p-3">
+    <div class="mb-3 fw-bold fs-3"><span class="text-success">ANTRIAN</span> P.SMA</div>
+    <div class="border border-success rounded-2 py-2 mb-4">
+      <!-- menampilkan informasi jumlah antrian -->
+      <h1 id="antrian" class="display-1 fw-bold text-success text-center lh-1 pb-2"></h1>
+    </div>
+    <!-- button pengambilan nomor antrian -->
+    <a id="insert_2" href="javascript:void(0)" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">
+      <i class="bi-person-plus fs-4 me-2"></i> Ambil Nomor
+    </a>
+  </div>
+</div>
         </div>
       </div>
     </div>
