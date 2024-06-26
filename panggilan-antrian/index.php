4a8a9da65111de928581d93fcbc7a5bfa682d062
muridsarwana <muridsarwana@gmail.com>
2024-06-26 15:45:16 +0700

    edit index panggilan antrian

--- panggilan-antrian/index.php
+++ panggilan-antrian/index.php
@@ -171,7 +171,8 @@
   <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.js"></script>
   <!-- Responsivevoice -->
   <!-- Get API Key -> https://responsivevoice.org/ -->
-  <script src="https://code.responsivevoice.org/responsivevoice.js?key=jQZ2zcdq"></script>
+  <!-- <script src="https://code.responsivevoice.org/responsivevoice.js?key=jQZ2zcdq"></script> -->
+  <script src="https://code.responsivevoice.org/responsivevoice.js?key=WxSg6wJK"></script>
 
   <script type="text/javascript">
     $(document).ready(function() {
