15167aae3ea33958941bf0687a09956c45282771
muridsarwana <muridsarwana@gmail.com>
2024-06-27 16:33:06 +0700

    add more stuff and edit index

--- nomor-antrian/index.php
+++ nomor-antrian/index.php
@@ -1,14 +1,3 @@
-<!-- Aplikasi Antrian Berbasis Web 
-**********************************************
-* Developer   : Indra Styawantoro
-* Company     : Indra Studio
-* Release     : Juni 2021
-* Update      : -
-* Website     : www.indrasatya.com
-* E-mail      : indra.setyawantoro@gmail.com
-* WhatsApp    : +62-821-8686-9898
--->
-
 <!doctype html>
 <html lang="en" class="h-100">
 
@@ -42,44 +31,118 @@
   <main class="flex-shrink-0">
     <div class="container pt-5">
       <div class="row justify-content-lg-center">
-        <div class="col-lg-5 mb-4">
-        <div class="px-4 py-3 mb-4 bg-white rounded-2 shadow-sm">
-  <!-- judul halaman -->
-  <div class="d-flex align-items-center me-md-auto">
-    <i class="bi-people-fill text-success me-3 fs-3"></i>
-    <h1 class="h5 pt-2">Nomor Antrian</h1>
-  </div>
-</div>
+        <div class="col-lg mb-4">
+          <div class="px-4 py-3 mb-4 bg-white rounded-2 shadow-sm">
+            <!-- judul halaman -->
+            <div class="d-flex align-items-center me-md-auto">
+              <i class="bi-people-fill text-success me-3 fs-3"></i>
+              <h1 class="h5 pt-2">Nomor Antrian</h1>
+            </div>
+          </div>
 
-<!-- Queue Section 1. Sekretariat -->
-<div class="card border-0 shadow-sm">
-  <div class="card-body text-center d-grid p-3">
-    <div class="mb-3 fw-bold fs-3"><span class="text-success">ANTRIAN</span> SEKRETARIAT</div>
-    <div class="border border-success rounded-2 py-2 mb-4">
-      <!-- menampilkan informasi jumlah antrian -->
-      <h1 id="antrian_sek" class="display-1 fw-bold text-success text-center lh-1 pb-2"></h1>
-    </div>
-    <!-- button pengambilan nomor antrian -->
-    <a id="insert_sek" href="javascript:void(0)" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">
-      <i class="bi-person-plus fs-4 me-2"></i> Ambil Nomor
-    </a>
-  </div>
-</div>
+          <!-- Queue Section -->
+          <div class="row mb-4">
+            <div class="col-lg-4">
+              <!-- 1. Sekretariat -->
+              <div class="card border-0 shadow-sm mb-4">
+                <div class="card-body text-center d-grid p-3">
+                  <div class="mb-3 fw-bold fs-3"><span class="text-success">ANTRIAN</span> SEKRETARIAT</div>
+                  <div class="border border-success rounded-2 py-2 mb-4">
+                    <!-- menampilkan informasi jumlah antrian -->
+                    <h1 id="antrian_1sek" class="display-1 fw-bold text-success text-center lh-1 pb-2"></h1>
+                  </div>
+                  <!-- button pengambilan nomor antrian -->
+                  <a id="insert_1sek" href="javascript:void(0)" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">
+                    <i class="bi-person-plus fs-4 me-2"></i> Ambil Nomor
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4">
+             <!-- 2. Bidang Pembinaan SMA -->
+              <div class="card border-0 shadow-sm mb-4">
+                <div class="card-body text-center d-grid p-3">
+                  <div class="mb-3 fw-bold fs-3"><span class="text-success">ANTRIAN</span> P.SMA</div>
+                  <div class="border border-success rounded-2 py-2 mb-4">
+                    <!-- menampilkan informasi jumlah antrian -->
+                    <h1 id="antrian_2sma" class="display-1 fw-bold text-success text-center lh-1 pb-2"></h1>
+                  </div>
+                  <!-- button pengambilan nomor antrian -->
+                  <a id="insert_2sma" href="javascript:void(0)" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">
+                    <i class="bi-person-plus fs-4 me-2"></i> Ambil Nomor
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <!-- 3. Bidang Pembinaan SMK -->
+              <div class="card border-0 shadow-sm mb-4">
+                <div class="card-body text-center d-grid p-3">
+                  <div class="mb-3 fw-bold fs-3"><span class="text-success">ANTRIAN</span> P.SMK</div>
+                  <div class="border border-success rounded-2 py-2 mb-4">
+                    <!-- menampilkan informasi jumlah antrian -->
+                    <h1 id="antrian_3smk" class="display-1 fw-bold text-success text-center lh-1 pb-2"></h1>
+                  </div>
+                  <!-- button pengambilan nomor antrian -->
+                  <a id="insert_3smk" href="javascript:void(0)" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">
+                    <i class="bi-person-plus fs-4 me-2"></i> Ambil Nomor
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
 
-<!-- Queue Section 2. PSMA -->
-<div class="card border-0 shadow-sm">
-  <div class="card-body text-center d-grid p-3">
-    <div class="mb-3 fw-bold fs-3"><span class="text-success">ANTRIAN</span> P.SMA</div>
-    <div class="border border-success rounded-2 py-2 mb-4">
-      <!-- menampilkan informasi jumlah antrian -->
-      <h1 id="antrian_sma" class="display-1 fw-bold text-success text-center lh-1 pb-2"></h1>
-    </div>
-    <!-- button pengambilan nomor antrian -->
-    <a id="insert_sma" href="javascript:void(0)" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">
-      <i class="bi-person-plus fs-4 me-2"></i> Ambil Nomor
-    </a>
-  </div>
-</div>
+          <!-- Second Row -->
+          <div class="row mb-4">
+            <div class="col-lg-4">
+              <!-- 4. Bidang Pembinaan DIKSUS -->
+              <div class="card border-0 shadow-sm mb-4">
+                <div class="card-body text-center d-grid p-3">
+                  <div class="mb-3 fw-bold fs-3"><span class="text-success">ANTRIAN</span> P.DIKSUS</div>
+                  <div class="border border-success rounded-2 py-2 mb-4">
+                    <!-- menampilkan informasi jumlah antrian -->
+                    <h1 id="antrian_4slb" class="display-1 fw-bold text-success text-center lh-1 pb-2"></h1>
+                  </div>
+                  <!-- button pengambilan nomor antrian -->
+                  <a id="insert_4slb" href="javascript:void(0)" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">
+                    <i class="bi-person-plus fs-4 me-2"></i> Ambil Nomor
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <!-- 5. Bidang Pembinaan Kebudayaan -->
+              <div class="card border-0 shadow-sm mb-4">
+                <div class="card-body text-center d-grid p-3">
+                  <div class="mb-3 fw-bold fs-3"><span class="text-success">ANTRIAN</span> KEBUDAYAAN</div>
+                  <div class="border border-success rounded-2 py-2 mb-4">
+                    <!-- menampilkan informasi jumlah antrian -->
+                    <h1 id="antrian_5bud" class="display-1 fw-bold text-success text-center lh-1 pb-2"></h1>
+                  </div>
+                  <!-- button pengambilan nomor antrian -->
+                  <a id="insert_5bud" href="javascript:void(0)" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">
+                    <i class="bi-person-plus fs-4 me-2"></i> Ambil Nomor
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <!-- 6. KETENAGAAN -->
+              <div class="card border-0 shadow-sm mb-4">
+                <div class="card-body text-center d-grid p-3">
+                  <div class="mb-3 fw-bold fs-3"><span class="text-success">ANTRIAN</span> KETENAGAAN</div>
+                  <div class="border border-success rounded-2 py-2 mb-4">
+                    <!-- menampilkan informasi jumlah antrian -->
+                    <h1 id="antrian_6ket" class="display-1 fw-bold text-success text-center lh-1 pb-2"></h1>
+                  </div>
+                  <!-- button pengambilan nomor antrian -->
+                  <a id="insert_6ket" href="javascript:void(0)" class="btn btn-success btn-block rounded-pill fs-5 px-5 py-4 mb-2">
+                    <i class="bi-person-plus fs-4 me-2"></i> Ambil Nomor
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -105,18 +168,18 @@
   <script type="text/javascript">
     $(document).ready(function() {
       // tampilkan jumlah antrian
-      $('#antrian_sek').load('get_antrian_sek.php');
+      $('#antrian_1sek').load('get_antrian_1sek.php');
 
       // proses insert data
-      $('#insert_sek').on('click', function() {
+      $('#insert_1sek').on('click', function() {
         $.ajax({
           type: 'POST',                     // mengirim data dengan method POST data sekretariat
-          url: 'insert_sek.php',                // url file proses insert data
+          url: 'insert_1sek.php',                // url file proses insert data
           success: function(result) {       // ketika proses insert data selesai
             // jika berhasil
             if (result === 'Sukses') {
               // tampilkan jumlah antrian
-              $('#antrian_sek').load('get_antrian_sek.php').fadeIn('slow');
+              $('#antrian_1sek').load('get_antrian_1sek.php').fadeIn('slow');
             }
           },
         });
@@ -128,18 +191,110 @@
   <script type="text/javascript">
     $(document).ready(function() {
       // tampilkan jumlah antrian
-      $('#antrian_sma').load('get_antrian_sma.php');
+      $('#antrian_2sma').load('get_antrian_2sma.php');
 
       // proses insert data
-      $('#insert_sma').on('click', function() {
+      $('#insert_2sma').on('click', function() {
         $.ajax({
           type: 'POST',                     // mengirim data dengan method POST data sma
-          url: 'insert_sma.php',                // url file proses insert data
+          url: 'insert_2sma.php',                // url file proses insert data
           success: function(result) {       // ketika proses insert data selesai
             // jika berhasil
             if (result === 'Sukses') {
               // tampilkan jumlah antrian
-              $('#antrian_sma').load('get_antrian_sma.php').fadeIn('slow');
+              $('#antrian_2sma').load('get_antrian_2sma.php').fadeIn('slow');
+            }
+          },
+        });
+      });
+    });
+  </script>
+
+    <!-- Handler Section 3. P.SMK -->
+    <script type="text/javascript">
+    $(document).ready(function() {
+      // tampilkan jumlah antrian
+      $('#antrian_3smk').load('get_antrian_3smk.php');
+
+      // proses insert data
+      $('#insert_3smk').on('click', function() {
+        $.ajax({
+          type: 'POST',                     // mengirim data dengan method POST data sma
+          url: 'insert_3smk.php',                // url file proses insert data
+          success: function(result) {       // ketika proses insert data selesai
+            // jika berhasil
+            if (result === 'Sukses') {
+              // tampilkan jumlah antrian
+              $('#antrian_3smk').load('get_antrian_3smk.php').fadeIn('slow');
+            }
+          },
+        });
+      });
+    });
+  </script>
+  
+    <!-- Handler Section 4. P.SLB -->
+    <script type="text/javascript">
+    $(document).ready(function() {
+      // tampilkan jumlah antrian
+      $('#antrian_4slb').load('get_antrian_4slb.php');
+
+      // proses insert data
+      $('#insert_4slb').on('click', function() {
+        $.ajax({
+          type: 'POST',                     // mengirim data dengan method POST data sma
+          url: 'insert_4slb.php',                // url file proses insert data
+          success: function(result) {       // ketika proses insert data selesai
+            // jika berhasil
+            if (result === 'Sukses') {
+              // tampilkan jumlah antrian
+              $('#antrian_4slb').load('get_antrian_4slb.php').fadeIn('slow');
+            }
+          },
+        });
+      });
+    });
+  </script>
+
+    <!-- Handler Section 5. Kebudayaan -->
+    <script type="text/javascript">
+    $(document).ready(function() {
+      // tampilkan jumlah antrian
+      $('#antrian_5bud').load('get_antrian_5bud.php');
+
+      // proses insert data
+      $('#insert_5bud').on('click', function() {
+        $.ajax({
+          type: 'POST',                     // mengirim data dengan method POST data sma
+          url: 'insert_5bud.php',                // url file proses insert data
+          success: function(result) {       // ketika proses insert data selesai
+            // jika berhasil
+            if (result === 'Sukses') {
+              // tampilkan jumlah antrian
+              $('#antrian_5bud').load('get_antrian_5bud.php').fadeIn('slow');
+            }
+          },
+        });
+      });
+    });
+  </script>
+
+      <!-- Handler Section 6. Ketenagaan -->
+      <script type="text/javascript">
+    $(document).ready(function() {
+      // tampilkan jumlah antrian
+      $('#antrian_6ket').load('get_antrian_6ket.php');
+
+      // proses insert data
+      $('#insert_6ket').on('click', function() {
+        $.ajax({
+          type: 'POST',                     // mengirim data dengan method POST data sma
+          url: 'insert_6ket.php',                // url file proses insert data
+          success: function(result) {       // ketika proses insert data selesai
+            // jika berhasil
+            if (result === 'Sukses') {
+              // tampilkan jumlah antrian
+              $('#antrian_6ket').load('get_antrian_6ket.php').fadeIn('slow');
             }
           },
         });
