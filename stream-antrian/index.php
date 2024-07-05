1ef0e0750c2619faafc0bc39f6220feba75f038d
muridsarwana <muridsarwana@gmail.com>
2024-07-05 14:51:00 +0700

    fix some feature

---
+++ stream-antrian/index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en" class="h-100">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Title -->
+  <title>Stream Antrian</title>
+  <!-- Favicon icon -->
+  <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <!-- Bootstrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  <!-- Font -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
+  <!-- Custom Style -->
+  <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<body class="d-flex flex-column h-100">
+  <main class="flex-shrink-0">
+    <div class="container pt-5">
+      <!-- tampilkan pesan selamat datang -->
+      <div class="alert alert-light d-flex align-items-center mb-5" role="alert">
+        <i class="bi-info-circle text-success me-3 fs-3"></i>
+        <div>
+          Antrian <strong>Pelayanan DISDIKBUD</strong>
+        </div>
+      </div>
+
+      <div class="row gx-3">
+        <!-- Card 1 -->
+        <div class="col-md-4 mb-4">
+          <div class="card border-0 shadow-sm h-100">
+            <div class="card-body p-5">
+              <!-- blank card -->
+                <div>
+                    Sing <strong>GEDE</strong>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card 2 -->
+        <div class="col-md-4 mb-4">
+          <div class="card border-0 shadow-sm h-100">
+            <div class="card-body p-5">
+              <!-- blank card -->
+                <div>
+                    Sing <strong>GEDE</strong>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card 3 -->
+        <div class="col-md-4 mb-4">
+          <div class="card border-0 shadow-sm h-100">
+            <div class="card-body p-5">
+              <!-- blank card -->
+                <div>
+                    Sing <strong>GEDE</strong>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card 4 -->
+        <div class="col-md-4 mb-4">
+          <div class="card border-0 shadow-sm h-100">
+            <div class="card-body p-5">
+              <!-- blank card -->
+                <div>
+                    Sing <strong>GEDE</strong>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card 5 -->
+        <div class="col-md-4 mb-4">
+          <div class="card border-0 shadow-sm h-100">
+            <div class="card-body p-5">
+              <!-- blank card -->
+                <div>
+                    Sing <strong>GEDE</strong>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Card 6 -->
+        <div class="col-md-4 mb-4">
+          <div class="card border-0 shadow-sm h-100">
+            <div class="card-body p-5">
+              <!-- blank card -->
+                <div>
+                    Sing <strong>GEDE</strong>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <!-- Popper and Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+</body>
+</html>
